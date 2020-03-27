219bb58a0759282cf4d4997e5c579dea6b527813
baileywickham <baileywickham@gmail.com>
2020-03-27 13:38:13 -0700

    reformat

--- README.md
+++ README.md
@@ -5,8 +5,16 @@ A minimal container implementation in C
 ## About
 newroot contains the new filesystem root. In my case it is a minimal alpine distro.
 
+## Overlayfs
+Overlayfs is used to create virtual file systems which will not be written to. This is done by using overlayfs with multiple lower dirs. Overlays may be composed as you would expect. This is used when multiple containers use the same base image.
+
+To restore a container, I think you would move the workdir to the upperdir.
+
 ## TODO
 - Figure out network namespaces
 - Figure out mount namespaces
 - Automount proc fs `mount -t proc proc /proc` *DONE*
 - Figure out clone/fork
+
+## Notes
+- https://github.com/riswandans/litesploit/blob/master/modules/exploits/linux/localroot/overlayfs.c
--- contd.c
+++ contd.c
@@ -1,25 +1,24 @@
 #define _GNU_SOURCE
-#include <string.h>
 #include <errno.h>
+#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <sched.h>
+#include <string.h>
 #include <sys/mount.h>
 #include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <syslog.h>
+#include <unistd.h>
 
 #include "contd.h"
 
-
 #define NEWROOT "newroot"
 #define OLDROOT "oldroot"
-#define MOUNT_DIR "./newroot"
+#define MOUNT_DIR "./workdir"
 
-
-void startcont() {
+void startcont()
+{
     char* args[1];
 
     pid_t pid = getpid();
@@ -36,12 +35,13 @@ void startcont() {
 
     args[0] = NULL;
     // exec init process
+    // TODO change to command
     execv("/sbin/init", args);
 }
 
-
-void unsharecont() {
-    const int UNSHARE_FLAGS = CLONE_NEWNET |CLONE_NEWNS | CLONE_NEWUTS | CLONE_NEWPID;
+void unsharecont()
+{
+    const int UNSHARE_FLAGS = CLONE_NEWNET | CLONE_NEWNS | CLONE_NEWUTS | CLONE_NEWPID;
     const char* newname = "cont"; // TODO change to proc name with hash
     if (unshare(UNSHARE_FLAGS) < 0) {
         perror("failed to change namespaces\n");
@@ -51,19 +51,22 @@ void unsharecont() {
         perror("failed to rename cont\n");
         exit(EXIT_FAILURE);
     }
+    if (mount("overlay", "./workdir", "overlayfs", MS_MGC_VAL, "lowerdir=./newroot,upperdir=./oldroot") < 0) {
+        perror("mount overlay failed");
+    }
     if (chroot(MOUNT_DIR) < 0 || chdir(MOUNT_DIR) < 0) {
         perror("chroot failed");
         exit(EXIT_FAILURE);
     }
     // TODO Figure out how this works
-   // if (mount("", "/", "none", MS_REC | MS_PRIVATE, NULL) < 0) {
-   //     perror("mount failed");
-   //     exit(EXIT_FAILURE);
-   // }
+    // if (mount("", "/", "none", MS_REC | MS_PRIVATE, NULL) < 0) {
+    //     perror("mount failed");
+    //     exit(EXIT_FAILURE);
+    // }
 }
 
-
-void createdaemon() {
+void createdaemon()
+{
     pid_t pid = fork();
     if (pid < 0) {
         perror("failed to fork daemon");
@@ -87,19 +90,17 @@ void createdaemon() {
     if (pid > 0) {
         exit(0); //kill parent
     }
-
 }
 
-
-void mountproc() {
+void mountproc()
+{
     if (mount("proc", "/proc", "proc", 0, NULL) < 0) {
         perror("failed to mount proc");
         exit(EXIT_FAILURE);
     }
 }
 
-void pvtroot() {
+void pvtroot()
+{
     // TODO decide whether to use pivot root or switchroot
 }
-
-
