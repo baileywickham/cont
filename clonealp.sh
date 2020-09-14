176ec1efe27fb70bcaabd8819cc05a1c61e1f683
baileywickham <baileywickham@gmail.com>
2020-09-13 20:53:54 -0700

    clean up alp

--- clonealp.sh
+++ clonealp.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/alpine-minirootfs-3.11.3-x86_64.tar.gz -O alp.tar.gz
-tar -xf alp.tar.gz -C ./newroot/
+mkdir -p alpine
+wget http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/alpine-minirootfs-3.11.3-x86_64.tar.gz -O alpine/alp.tar.gz
+tar -xf alpine/alp.tar.gz -C ./alpine/
